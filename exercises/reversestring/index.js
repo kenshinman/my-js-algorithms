8f14d77da5ca9f8c8de56dd832bd9dd04ea1fec4
Kehinde Orilogbon <archkenny@yahoo.com>
2019-08-15 19:30:20 +0100

    string reversal

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,21 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let reversed = "";
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+
+  return reversed;
+}
 
 module.exports = reverse;
+
+//solution 1
+// function reverse(str) {
+//   return str
+//     .split("")
+//     .reverse()
+//     .join("");
+// }
